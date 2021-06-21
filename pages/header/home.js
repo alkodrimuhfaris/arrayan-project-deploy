c9418e5a8ad24c4a5deea938e335573b60e5761a
alkodrimuhfaris <farisalkodri@gmail.com>
2021-06-21 00:42:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/header/home.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function HomeHeader() {
+  return (
+    <div>
+      <Head>
+        <title>Home Arrayan</title>
+        <meta property="og:title" content="Home Page: Arrayan" key="title" />
+      </Head>
+    </div>
+  );
+}
+
+export default HomeHeader;
