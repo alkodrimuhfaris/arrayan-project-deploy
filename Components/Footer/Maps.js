3db4d3dcb7f771915bc1db1ffa0ba2792b6f5e5d
alkodrimuhfaris <farisalkodri@gmail.com>
2021-07-11 13:10:02 +0700

    Add: static google maps

--- Components/Footer/Maps.js
+++ Components/Footer/Maps.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from 'react-google-maps';
-// import maps from '../../Assets/Photos/GoogleMaps.png';
+// import {
+//   withScriptjs,
+//   withGoogleMap,
+//   GoogleMap,
+//   Marker,
+// } from 'react-google-maps';
+import {StaticGoogleMap, Marker, Path} from 'react-static-google-map';
+import getComponentWidth from '../../componentHelpers/getComponentWidth';
 
 export default function Maps({
   markers = [
@@ -14,40 +15,54 @@ export default function Maps({
     {lat: -6.1646619, lng: 107.2672309},
   ],
 }) {
-  const MapComponent = withScriptjs(
-    withGoogleMap((props) => (
-      <GoogleMap defaultZoom={10} defaultCenter={markers[0]}>
-        {props.isMarkerShown &&
-          markers.map((val, idx) => <Marker key={idx} position={val} />)}
-      </GoogleMap>
-    )),
-  );
+  const ref1 = React.useRef(null);
+  const [wRef1, hRef1] = getComponentWidth(ref1);
+  // const MapComponent = withScriptjs(
+  //   withGoogleMap((props) => (
+  //     <GoogleMap defaultZoom={10} defaultCenter={markers[0]}>
+  //       {props.isMarkerShown &&
+  //         markers.map((val, idx) => <Marker key={idx} position={val} />)}
+  //     </GoogleMap>
+  //   )),
+  // );
 
   return (
-    <MapComponent
-      //
-      isMarkerShown
-      googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_APP_API_KEY_GMAPS}&v=3.exp&libraries=geometry,drawing,places`}
-      loadingElement={
-        <div
-          style={{
-            marginTop: '4em',
-            width: `100%`,
-            height: `calc(100vw/2.66667)`,
-          }}
-        />
-      }
-      containerElement={
-        <div
-          style={{
-            marginTop: '4em',
-            width: `100%`,
-            height: `calc(100vw/2.66667)`,
-          }}
-        />
-      }
-      mapElement={<div style={{height: `100%`}} />}
-    />
+    <section ref={ref1} className="maps mt-5">
+      {/* <MapComponent
+        //
+        isMarkerShown
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_APP_API_KEY_GMAPS}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={
+          <div
+            style={{
+              width: `100%`,
+              height: `calc(100vw/2.66667)`,
+            }}
+          />
+        }
+        containerElement={
+          <div
+            style={{
+              width: `100%`,
+              height: `calc(100vw/2.66667)`,
+            }}
+          />
+        }
+        mapElement={<div style={{height: `100%`}} />}
+      /> */}
+      <StaticGoogleMap
+        zoom={10}
+        size={`${wRef1}x${hRef1}`}
+        scale={4}
+        apiKey={process.env.NEXT_PUBLIC_APP_API_KEY_GMAPS}
+      >
+        <Marker.Group label="Arrayan Residence" color="brown">
+          {markers.map((val, index) => (
+            <Marker key={index} location={val} />
+          ))}
+        </Marker.Group>
+      </StaticGoogleMap>
+    </section>
     // <section id="locationsArrayan" className="maps p-0 mt-4">
     // </section>
   );
