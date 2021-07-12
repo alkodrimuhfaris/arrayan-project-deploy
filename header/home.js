d2b4f70be96bf0da913a99dbaaddcdf3b4d48819
alkodrimuhfaris <farisalkodri@gmail.com>
2021-07-12 10:24:25 +0700

    Add metatags for image, facebook, and twitter

--- header/home.js
+++ header/home.js
@@ -3,10 +3,9 @@ import Head from 'next/head';
 function HomeHeader() {
   return (
     <Head>
+      {/* <!-- Primary Meta Tags --> */}
       <title>
-        Arrayan Group | Pusat Rumah Murah Bersubsidi Di Daerah Ibukota, Dapatkan
-        Unit Super Murah di Grand Cikarang City 2, Grand Vista Cikarang, dan
-        Villa Kencana Cikarang
+        Arrayan Group | Pusat Rumah Murah Bersubsidi Di Daerah Ibukota
       </title>
       <meta
         property="og:title"
@@ -17,6 +16,39 @@ function HomeHeader() {
         name="description"
         content="Pusat Rumah Murah Subsidi Di Daerah Ibukota. Kunjungi Wesite Kami Untuk Rumah Murah Bersubsidi Yang Nyaman Dengan Fasilitas Terbaik Di Kelasnya. Berlokasi di Bekasi, Karawang, dan Purwakarta. Dapatkan Unit Super Murah di Grand Cikarang City 2, Grand Vista Cikarang, dan Villa Kencana Cikarang"
       />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
+      <meta
+        property="og:title"
+        content="Arrayan Group | Pusat Rumah Murah Bersubsidi Di Daerah Ibukota"
+      />
+      <meta
+        property="og:description"
+        content="Pusat Rumah Murah Subsidi Di Daerah Ibukota. Kunjungi Wesite Kami Untuk Rumah Murah Bersubsidi Yang Nyaman Dengan Fasilitas Terbaik Di Kelasnya. Berlokasi di Bekasi, Karawang, dan Purwakarta. Dapatkan Unit Super Murah di Grand Cikarang City 2, Grand Vista Cikarang, dan Villa Kencana Cikarang"
+      />
+      <meta
+        property="og:image"
+        content="https://arrayan-engine.lihar.id/storage/slider/11de60f1febf4f9a9c79d9780f43a900.jpg"
+      />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={process.env.NEXT_PUBLIC_URL} />
+      <meta
+        property="twitter:title"
+        content="Arrayan Group | Pusat Rumah Murah Bersubsidi Di Daerah Ibukota"
+      />
+      <meta
+        property="twitter:description"
+        content="Pusat Rumah Murah Subsidi Di Daerah Ibukota. Kunjungi Wesite Kami Untuk Rumah Murah Bersubsidi Yang Nyaman Dengan Fasilitas Terbaik Di Kelasnya. Berlokasi di Bekasi, Karawang, dan Purwakarta. Dapatkan Unit Super Murah di Grand Cikarang City 2, Grand Vista Cikarang, dan Villa Kencana Cikarang"
+      />
+      <meta
+        property="twitter:image"
+        content="https://arrayan-engine.lihar.id/storage/slider/11de60f1febf4f9a9c79d9780f43a900.jpg"
+      />
+
       <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
       <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
       <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
