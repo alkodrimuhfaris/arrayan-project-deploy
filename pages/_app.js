1837659e46a6eb69cc4781b53f6a1a2c5da2550d
alkodrimuhfaris <farisalkodri@gmail.com>
2021-07-30 20:01:26 +0700

    Add: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import {Provider} from 'react-redux';
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
+import * as gtag from '../lib/gtag';
 import store from '../redux/store';
 import '../styles/globals.css';
 import '../styles/globals.scss';
@@ -12,12 +15,23 @@ import '../styles/IconStyle.scss';
 import '../styles/ModalLoading.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyApp({Component, pageProps}) {
+const App = ({Component, pageProps}) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
-}
+};
 
-export default MyApp;
+export default App;
