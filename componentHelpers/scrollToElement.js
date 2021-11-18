f461b27b9d3b70ac08433eece5e82b79f483dd69
alkodrimuhfaris <farisalkodri@gmail.com>
2021-11-18 17:54:05 +0700

    edit component helpers

--- componentHelpers/scrollToElement.js
+++ componentHelpers/scrollToElement.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {useRouter} from 'next/router';
-import {useSelector} from 'react-redux';
 import useWindowDimensions from './getWindowDimensions';
 
 export default function scrollToElement({jumpElement = ''}) {
